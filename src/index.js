8bcd1264516a84816429b78f4badd875066d5b35
Redvan <redvanisation@gmail.com>
2019-12-19 22:59:26 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className="title">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.querySelector('#root'),
+);
