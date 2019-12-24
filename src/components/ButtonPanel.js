94d8272c01c7a221ec08102ca916be3e930e7799
Redvan <redvanisation@gmail.com>
2019-12-24 10:40:38 -0500

    refactoring calculate.js ButtonPanel component and enabling linter

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,45 +1,29 @@
-/* eslint-disable arrow-body-style */
 import React from 'react';
 import Button from './Button';
 
-const ButtonPanel = () => {
-  return (
-    <div className="calculator__panel">
-      <div id="group-1" className="calculator__panel--buttonsRow">
-        <Button buttonName="AC" color="gray" />
-        <Button buttonName="+/-" color="gray" />
-        <Button buttonName="%" color="gray" />
-        <Button buttonName="/" />
-      </div>
+const values = [['AC', '+/-', '%', '/'], ['7', '8', '9', 'X'],
+  ['4', '5', '6', '-'],
+  ['1', '2', '3', '+'],
+  ['0', '.', '=']];
 
-      <div id="group-2" className="calculator__panel--buttonsRow">
-        <Button buttonName="7" color="gray" />
-        <Button buttonName="8" color="gray" />
-        <Button buttonName="9" color="gray" />
-        <Button buttonName="X" />
+const ButtonPanel = () => (
+  <div className="calculator__panel">
+    {values.map((value) => (
+      <div key={value} className="calculator__panel--buttonsRow">
+        {value.map((val) => {
+          let colors;
+          let wide = '';
+          if (value.indexOf(val) === value.length - 1) {
+            colors = 'orange';
+          } else {
+            colors = 'gray';
+          }
+          wide = val === '0' ? wide = true : false;
+          return <Button key={val} buttonName={val} color={colors} wide={wide} />;
+        })}
       </div>
-
-      <div id="group-3" className="calculator__panel--buttonsRow">
-        <Button buttonName="4" color="gray" />
-        <Button buttonName="5" color="gray" />
-        <Button buttonName="6" color="gray" />
-        <Button buttonName="-" />
-      </div>
-
-      <div id="group-4" className="calculator__panel--buttonsRow">
-        <Button buttonName="1" color="gray" />
-        <Button buttonName="2" color="gray" />
-        <Button buttonName="3" color="gray" />
-        <Button buttonName="+" />
-      </div>
-
-      <div id="group-5" className="calculator__panel--buttonsRow">
-        <Button buttonName="0" color="gray" wide />
-        <Button buttonName="." color="gray" />
-        <Button buttonName="=" />
-      </div>
-    </div>
-  );
-};
+    ))}
+  </div>
+);
 
 export default ButtonPanel;
