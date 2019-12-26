df8817d801f6aa79bf3c225532eed3aad8f7acc8
Redvan <redvanisation@gmail.com>
2019-12-26 12:27:21 -0500

    Fixing the bug that appears if not two numbers are selected

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,8 +2,9 @@
 const Big = require('big.js');
 
 const operate = (numberOne, numberTwo, operation) => {
-  const num1 = new Big(numberOne);
-  const num2 = new Big(numberTwo);
+  if (!numberOne || !numberTwo) return 'Please Select two numbers';
+  const num1 = Big(numberOne);
+  const num2 = Big(numberTwo);
   switch (operation) {
     case '+':
       return num1.plus(num2).toString();
